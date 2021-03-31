a5f6fd15c2a0678db5e0d6f8df3227c9a753c4c1
folksgl <Gfolks14@gmail.com>
2021-03-31 17:29:27 -0400

    Correct test url

--- api/tests.py
+++ api/tests.py
@@ -8,6 +8,6 @@ class HelloWorldTest(SimpleTestCase):
     """ Hello world view tests """
 
     def test_hello_world(self):
-        response = self.client.get("/usps/hello")
+        response = self.client.get("/hello")
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(json.loads(response.getvalue()), {"message": "Hello, world!"})
