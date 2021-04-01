cf5c76f92aaa5b2ae4cc13cd85f7efeea24cc920
folksgl <Gfolks14@gmail.com>
2021-04-01 15:14:28 -0400

    Add asynchronous transaction logging

---
+++ api/transaction_log.py
@@ -0,0 +1,69 @@
+""" Transaction Logging functionality """
+import logging
+import os
+import aiohttp
+from django.conf import settings
+from django.http import JsonResponse
+
+# TRANSACTION_URL = "http://localhost:8000/transaction/"
+TRANSACTION_ROUTE = os.environ.get("TRANSACTION_ROUTE")
+TRANSACTION_URL = f"http://{TRANSACTION_ROUTE}:8080/transaction/"
+
+_DEBUG_RESPONSE = JsonResponse({"record_uuid": "fake-uuid"})
+
+transaction_unavailable_response = JsonResponse(
+    {
+        "error": "Transaction service temporarily unavailable. Check system logs for more information."
+    },
+    status=503,
+)
+
+
+async def create_transaction(csp: str, cost=0, result=None) -> JsonResponse:
+    """
+    Log a transaction to the transaction logging microservice.
+    Returns the transaction log information on success, otherwise a 503 response
+    defined by transaction_unavailable_response.
+    """
+    if settings.DEBUG:
+        logging.debug("Skipping transaction logging while in debug mode")
+        return _DEBUG_RESPONSE  # Skip sending a transaction log in debug mode
+
+    payload = {
+        "service_type": "PROOFING SERVICE",
+        "provider": "idemia",
+        "csp": csp,
+        "cost": cost,
+        "result": result,
+    }
+
+    try:
+        async with aiohttp.request("POST", TRANSACTION_URL, json=payload) as response:
+            transaction_result = await response.json()
+    except aiohttp.ClientError as error:
+        logging.error("Create transaction request raised exception: %s", error)
+        return transaction_unavailable_response
+
+    return JsonResponse(transaction_result)
+
+
+async def update_transaction_result(record_uuid: str, result: str) -> JsonResponse:
+    """
+    Update information in an existing transaction record
+    Returns the transaction log information on success, otherwise a 503 response
+    defined by transaction_unavailable_response.
+    """
+    if settings.DEBUG:
+        logging.debug("Skipping transaction logging update while in debug mode")
+        return _DEBUG_RESPONSE  # Skip sending a transaction log in debug mode
+
+    url = f"{TRANSACTION_URL}{record_uuid}/"
+
+    try:
+        async with aiohttp.request("PATCH", url, json={"result": result}) as response:
+            transaction_result = await response.json()
+    except aiohttp.ClientError as error:
+        logging.error("Update transaction raised exception: %s", error)
+        return transaction_unavailable_response
+
+    return JsonResponse(transaction_result)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,14 @@
 """ Views for USPS API """
 from django.http import JsonResponse
+from api import transaction_log
 
 
-async def hello_world(_request):
+async def hello_world(request):
     """ Hello world view """
+    csp_id = request.META["HTTP_X_CONSUMER_CUSTOM_ID"]
+    log_response = await transaction_log.create_transaction(csp_id)
+
+    if log_response.status_code != 200:
+        return log_response
+
     return JsonResponse({"message": "Hello, world!"})
