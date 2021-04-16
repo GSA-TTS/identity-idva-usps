e8969de6dedde9de4156e78df69105591f1f2207
Garrett Folks <garrett.folks@gsa.gov>
2021-04-16 11:00:17 -0400

    feature/pass request (#8)
    * Remove unused code
    
    * Pass request to USPS without repackaging

--- api/transaction_log.py
+++ api/transaction_log.py
@@ -1,15 +1,12 @@
 """ Transaction Logging functionality """
 import logging
-import os
 import aiohttp
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 # TRANSACTION_URL = "http://localhost:8000/transaction/"
-TRANSACTION_ROUTE = os.environ.get("TRANSACTION_ROUTE")
+TRANSACTION_ROUTE = settings.TRANSACTION_ROUTE
 TRANSACTION_URL = f"http://{TRANSACTION_ROUTE}:8080/transaction/"
-
-DEBUG_RESPONSE = JsonResponse({"record_uuid": "fake-uuid"})
 
 transaction_unavailable_response = JsonResponse(
     {
@@ -19,16 +16,14 @@ transaction_unavailable_response = JsonResponse(
 )
 
 
-async def create_transaction(csp: str, cost=0, result=None) -> JsonResponse:
+async def create_transaction(
+    session: aiohttp.ClientSession, csp: str, cost=0, result=None
+) -> HttpResponse:
     """
     Log a transaction to the transaction logging microservice.
     Returns the transaction log information on success, otherwise a 503 response
     defined by transaction_unavailable_response.
     """
-    if settings.DEBUG:
-        logging.debug("Skipping transaction logging while in debug mode")
-        return DEBUG_RESPONSE  # Skip sending a transaction log in debug mode
-
     payload = {
         "service_type": "PROOFING SERVICE",
         "provider": "idemia",
@@ -38,32 +33,10 @@ async def create_transaction(csp: str, cost=0, result=None) -> JsonResponse:
     }
 
     try:
-        async with aiohttp.request("POST", TRANSACTION_URL, json=payload) as response:
-            transaction_result = await response.json()
+        async with session.post(TRANSACTION_URL, json=payload) as response:
+            response.raise_for_status()
+            body = await response.read()
+            return HttpResponse(body, status=response.status)
     except aiohttp.ClientError as error:
         logging.error("Create transaction request raised exception: %s", error)
         return transaction_unavailable_response
-
-    return JsonResponse(transaction_result)
-
-
-async def update_transaction_result(record_uuid: str, result: str) -> JsonResponse:
-    """
-    Update information in an existing transaction record
-    Returns the transaction log information on success, otherwise a 503 response
-    defined by transaction_unavailable_response.
-    """
-    if settings.DEBUG:
-        logging.debug("Skipping transaction logging update while in debug mode")
-        return DEBUG_RESPONSE  # Skip sending a transaction log in debug mode
-
-    url = f"{TRANSACTION_URL}{record_uuid}/"
-
-    try:
-        async with aiohttp.request("PATCH", url, json={"result": result}) as response:
-            transaction_result = await response.json()
-    except aiohttp.ClientError as error:
-        logging.error("Update transaction raised exception: %s", error)
-        return transaction_unavailable_response
-
-    return JsonResponse(transaction_result)
