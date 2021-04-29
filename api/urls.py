d9ced2c557e2ecb4ec541dcfe6587a6d459f2dd4
shumatepf <shumatepfs@gmail.com>
2021-04-29 16:41:51 -0400

    Remove Django app

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-""" USPS API app url definitions """
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path(
-        "confidenceindicator", views.confidence_indicator, name="confidence_indicator"
-    ),
-]
